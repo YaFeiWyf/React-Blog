0f6352e2e3a407e95fdd5f632569339bf7c058b4
wangyafei <wangyafei@startimes.com.cn>
2016-11-15 21:02:32 +0800

    add disqusCounter

--- client/components/BlogItem/BlogItem.jsx
+++ client/components/BlogItem/BlogItem.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import NavLink from '../NavLink/NavLink';
 import {browserHistory} from 'react-router';
 import {dateFormat} from '../../utils/util';
+import ReactDisqusCounter from 'react-disqus-counter';
 require('./index.css');
 
 export default class BlogItem extends Component {
@@ -24,7 +25,14 @@ export default class BlogItem extends Component {
                     <NavLink to={'/blog/' + blogData.id}>
                         <h1>{blogData.title}</h1>
                         <p className='blogContent'>{blogData.plaintext}</p>
-                        <p className='blogInfo'>{dateFormat(blogData.publishDate)+'发布'} | {blogData.author}</p>
+                        <p className='blogInfo'>
+                            {dateFormat(blogData.publishDate)+'发布'} |
+                            {blogData.author} |
+                            <ReactDisqusCounter
+                            url={'http://blog.yvanwang.com/blog/'+blogData.id}
+                            shortname="http-www-yvanwang-com"/>
+                            条评论
+                        </p>
                     </NavLink>
                     { showEdit ? <span className="editButton" onClick={this.editButtonClick.bind(this)}>编辑</span> : null }
                 </li>
